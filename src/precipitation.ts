d3538eb8d76314193af0ceda19a3e99a6c84c9d0
Katy DeCorah <katydecorah@gmail.com>
2022-02-04 13:45:45 -0500

    nbsp

--- src/precipitation.ts
+++ src/precipitation.ts
@@ -41,7 +41,7 @@ export function listHourly({
   temperature,
   icon,
 }: Datum) {
-  const spacer = unixToHour(time).length === 4 ? " " : "";
+  const spacer = unixToHour(time).length === 4 ? "&nbsp;" : "";
   return `${getIcon(icon)} ${spacer}${unixToHour(
     time
   )}\t${precipAccumulation.toFixed(1)}" ${temperature.toFixed(0)}℉`;
